0d13699ba6078a0b8245a968b2bedcfb5ee4d7e1
Atharva-CM <atharva.mahamuni@bluepineapple.io>
2025-01-31 21:52:08 +0530

    feat: add configs and header footer components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+
+import { Geist, Geist_Mono } from "next/font/google";
+import NextTopLoader from "nextjs-toploader";
+import NavbarSection from "@/components/sections/navbar/default";
+import FooterSection from "@/components/sections/footer/default";
+import { ThemeProvider } from "@/components/contexts/theme-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,11 +28,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="" suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} bg-background antialiased`}
       >
-        {children}
+        <ThemeProvider>
+          <NextTopLoader />
+          <NavbarSection />
+          {children}
+          <FooterSection />
+        </ThemeProvider>
       </body>
     </html>
   );
